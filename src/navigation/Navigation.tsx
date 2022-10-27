9b26c1004e7777c499497769a7f17b7c9ed7f72f
Alex Dana <cadsdanaa@gmail.com>
2022-10-27 18:32:34 -0500

    Trying some updates to get mobile download of Resume working. Works fine on desktop.

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -21,7 +21,7 @@ export function Navigation() {
                 return <>
                     <a style={{color: '#39ff14'}} target={'blank'} href="https://github.com/cadsdanaa">Github</a>&nbsp;&nbsp;
                     <a style={{color: '#39ff14'}} target={'blank'} href="https://linkedin.com/in/alex-dana-907a74193">LinkedIn</a>&nbsp;&nbsp;
-                    <a style={{color: '#39ff14'}} download href="Resume.pdf">Resume</a>
+                    <a style={{color: '#39ff14'}} download href="Resume.pdf" target={'_self'}>Resume</a>
                 </>;
             case 'projects':
                 return 'Projects';
