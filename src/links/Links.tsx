56d6cd846df2a87c37cbc64a40ceada9c7d33d11
Alex Dana <cadsdanaa@gmail.com>
2022-10-29 01:09:13 -0500

    Moving resume back. Updated some rewrites in amplify as per https://stackoverflow.com/questions/68507437/downloading-a-pdf-file-in-reactjs-works-locally-but-doesnt-work-on-amazon-ampli

--- src/links/Links.tsx
+++ src/links/Links.tsx
@@ -4,6 +4,6 @@ export function Links() {
     return <>
         <a style={{color: '#39ff14'}} target={'blank'} href="https://github.com/cadsdanaa">Github</a>&nbsp;&nbsp;
         <a style={{color: '#39ff14'}} target={'blank'} href="https://linkedin.com/in/alex-dana-907a74193">LinkedIn</a>&nbsp;&nbsp;
-        <a style={{color: '#39ff14'}} download href="Resume.pdf" target={'_self'}>Resume</a>
+        <a style={{color: '#39ff14'}} download target="_blank" rel="noreferrer" href="Resume.pdf">Resume</a>
     </>
 }
