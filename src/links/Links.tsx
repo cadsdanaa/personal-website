a737daed6f2d7ccbb21e4a587cfdd84034132105
Alex Dana <cadsdanaa@gmail.com>
2022-10-29 01:19:02 -0500

    Removed duplicate resume. Updated links to get rid of hardcoded style for link color

--- src/links/Links.tsx
+++ src/links/Links.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import './Links.css';
 
 export function Links() {
     return <>
-        <a style={{color: '#39ff14'}} target={'blank'} href="https://github.com/cadsdanaa">Github</a>&nbsp;&nbsp;
-        <a style={{color: '#39ff14'}} target={'blank'} href="https://linkedin.com/in/alex-dana-907a74193">LinkedIn</a>&nbsp;&nbsp;
-        <a style={{color: '#39ff14'}} download target="_blank" rel="noreferrer" href="Resume.pdf">Resume</a>
+        <a target={'blank'} href="https://github.com/cadsdanaa">Github</a>&nbsp;&nbsp;
+        <a target={'blank'} href="https://linkedin.com/in/alex-dana-907a74193">LinkedIn</a>&nbsp;&nbsp;
+        <a download target="_blank" rel="noreferrer" href="Resume.pdf">Resume.pdf</a>
     </>
 }
