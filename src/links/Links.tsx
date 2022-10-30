7843f99b8de7dc178e15a51d189ec11373e88e51
Alex Dana <cadsdanaa@gmail.com>
2022-10-30 01:36:46 -0500

    Fixed issue with links not rendering properly. TODO is add command/typing ability for these rather than just click ability

--- src/links/Links.tsx
+++ src/links/Links.tsx
@@ -5,8 +5,17 @@ export const LINKS_LENGTH = 24;
 
 export function Links() {
     return <>
-        <a target={'blank'} href="https://github.com/cadsdanaa">Github</a>&nbsp;&nbsp;
-        <a target={'blank'} href="https://linkedin.com/in/alex-dana-907a74193">LinkedIn</a>&nbsp;&nbsp;
-        <a download target="_blank" rel="noreferrer" href="Resume.pdf">Resume.pdf</a>
+        Github&nbsp;&nbsp;
+        <a className={'Github'} target={'blank'} href="https://github.com/cadsdanaa">
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        </a>
+        LinkedIn&nbsp;&nbsp;
+        <a className={'LinkedIn'} target={'blank'} href="https://linkedin.com/in/alex-dana-907a74193">
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        </a>
+        Resume
+        <a className={'Resume'} download target="_blank" rel="noreferrer" href="Resume.pdf">
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        </a>
     </>
 }
