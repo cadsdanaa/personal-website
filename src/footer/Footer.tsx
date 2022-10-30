79c23fe4fd55d09399ae07cc1b358ddd2df57702
Alex Dana <cadsdanaa@gmail.com>
2022-10-30 11:59:51 -0500

    Added github link to latest commit for terminal version on click

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -2,7 +2,14 @@ import React from 'react';
 import './Footer.css';
 
 export function Footer() {
-    return <div className={'Footer'}>
+
+    const navigateToCommit = () => {
+        if(process.env.REACT_APP_COMMIT_ID) {
+            window.open('https://github.com/cadsdanaa/personal-website/commit/' + process.env.REACT_APP_COMMIT_ID);
+        }
+    }
+
+    return <div className={'Footer'} onClick={() => navigateToCommit()}>
         Terminal Version: {process.env.REACT_APP_COMMIT_ID ? process.env.REACT_APP_COMMIT_ID : 'local'}
     </div>
 }
