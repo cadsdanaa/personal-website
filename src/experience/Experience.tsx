1fc6a0965a990faed3bfc1d8a2f791a8c74ca06f
cadsdanaa <cadsdanaa@gmail.com>
2023-10-26 21:55:32 -0500

    Gotta update that experience.

--- src/experience/Experience.tsx
+++ src/experience/Experience.tsx
@@ -4,9 +4,9 @@ export const EXPERIENCE_LENGTH = 183;
 
 export function Experience() {
     return <>
-        <>- 6+ years working in software engineering</><br/>
-        <>- 1+ years AWS experience (CDK, Lambda, Step Functions, EventBridge, etc.)</><br/>
+        <>- 8+ years working in software engineering</><br/>
+        <>- 2+ years AWS experience (CDK, Lambda, SQS, EventBridge, etc.)</><br/>
         <>- 6+ years Java & Spring experience</><br/>
-        <>- 2+ years Typescript experience</><br/>
+        <>- 3+ years Typescript experience</><br/>
     </>;
 }
