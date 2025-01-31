09911af06e0dec83e7989353931be750634720f0
cadsdanaa <cadsdanaa@gmail.com>
2025-01-30 22:52:02 -0600

    Update Experience.tsx

--- src/experience/Experience.tsx
+++ src/experience/Experience.tsx
@@ -4,9 +4,9 @@ export const EXPERIENCE_LENGTH = 183;
 
 export function Experience() {
     return <>
-        <>- 8+ years working in software engineering</><br/>
-        <>- 2+ years AWS experience (CDK, Lambda, SQS, EventBridge, etc.)</><br/>
+        <>- 9+ years working in software engineering</><br/>
+        <>- 3+ years AWS experience (CDK, Lambda, SQS, EventBridge, etc.)</><br/>
         <>- 6+ years Java & Spring experience</><br/>
-        <>- 3+ years Typescript experience</><br/>
+        <>- 4+ years Typescript experience</><br/>
     </>;
 }
